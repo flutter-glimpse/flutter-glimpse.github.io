b916032e63e4a3dbf152a61f6deeab12642b94e2
Tomas <tomas.dostal@camelotglobal.com>
2019-02-09 00:47:15 +0000

    Updates

---
+++ precache-manifest.3975cf8715fbe95a6fd515236ca7f594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c0d6b6df2378b78fc6f0",
+    "url": "/static/js/main.c0d6b6df.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "c0d6b6df2378b78fc6f0",
+    "url": "/static/css/main.17a25b57.chunk.css"
+  },
+  {
+    "revision": "a3640d777e7da4551f3f33fe4dee7954",
+    "url": "/index.html"
+  }
+];
